d5262aca5d16de6f6c42c44721ed2088febe90aa
email <wolverine89@bk.ru>
2023-03-08 18:44:31 +0300

    second commit

---
+++ lesson_1/task_1/src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int[] array = {1, 1, 0, 1, 1, 1};
+        int[] y = new int[10];
+
+        int curr = 0;
+        int best = 0;
+        for (int i = 0; i < array.length; i++) {
+            int a = array[i];
+            if (a == 1) {
+                curr++;
+
+            } else {
+                if (curr > best) {
+                    best = curr;
+                }
+                curr = 0;
+            }
+        }
+        if (curr > best) {
+            best = curr;
+        }
+
+        System.out.println(best);
+    }
+}
